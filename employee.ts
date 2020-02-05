5ea0d21a6cbd14a080906cdbb1e8fde097c1b23f
AmolHelkar <26343613+Amolll@users.noreply.github.com>
2020-02-05 23:28:08 +0530

    Add files via upload

---
+++ employee.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+
+    selector:'<employee></employee>',
+    template:'<h1>Employee Component</h1>'
+})
+export class EmployeeComponent{
+
+}
